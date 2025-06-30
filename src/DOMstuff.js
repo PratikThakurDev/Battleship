d406c9e5c570adab8c49278a7c123acacb4f9877
PratikThakurDev <149695885+PratikThakurDev@users.noreply.github.com>
2025-06-30 12:02:04 +0000

    Added fuction to show hits and miss

--- src/DOMstuff.js
+++ src/DOMstuff.js
@@ -1,4 +1,4 @@
-import { shipPlacement,GameBoard } from './gameSetup';
+import { shipPlacement, GameBoard } from './gameSetup';
 
 const visualBoard = (board) => {
   for (let i = 0; i < 10; i++) {
@@ -14,7 +14,7 @@ const visualBoard = (board) => {
 };
 
 const toShowShips = (board) => {
-  const buckets = board
+  const buckets = board;
   const playerBoard = document.querySelector('#player-board');
   const cells = playerBoard.querySelectorAll('.cell');
   for (let i = 0; i < buckets.length; i++) {
@@ -30,4 +30,38 @@ const toShowShips = (board) => {
   }
 };
 
-export { visualBoard, toShowShips };
+const toShowHits = (board, activePlayerBoard) => {
+  const buckets = board;
+
+  const cells = activePlayerBoard.querySelectorAll('.cell');
+  for (let i = 0; i < buckets.length; i++) {
+    for (let j = 0; j < buckets.length; j++) {
+      if (buckets[i][j][1] === 'H') {
+        cells.forEach((cell) => {
+          if (cell.dataset.row == i && cell.dataset.column == j) {
+            cell.classList.add('hit');
+          }
+        });
+      }
+    }
+  }
+};
+
+const toShowMiss = (board, activePlayerBoard) => {
+  const buckets = board;
+
+  const cells = activePlayerBoard.querySelectorAll('.cell');
+  for (let i = 0; i < buckets.length; i++) {
+    for (let j = 0; j < buckets.length; j++) {
+      if (buckets[i][j][1] === 'M') {
+        cells.forEach((cell) => {
+          if (cell.dataset.row == i && cell.dataset.column == j) {
+            cell.classList.add('miss');
+          }
+        });
+      }
+    }
+  }
+};
+
+export { visualBoard, toShowShips, toShowHits, toShowMiss };
